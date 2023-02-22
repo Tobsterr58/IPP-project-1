191dac564f303884a90fa9ef191ecccca88b41aa
Tobiáš <tobste58@gmail.com>
2023-02-22 20:44:24 +0100

    First commit, about checking arguments.

---
+++ parser.php
@@ -0,0 +1,11 @@
+<?php
+    ini_set('display_errors', 'stderr');
+
+    if ($argc > 1) {
+        if ($argv[2] == "--help") {
+            echo "Usage: php parser.php [file] [options]
+            exit(0);
+        }
+    }
+        
+?>
